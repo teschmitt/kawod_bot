8b6d5923b752f31f40ebe9b82bcee0cb2a25cbf7
mrgnth <teschmitt@gmail.com>
2017-03-30 20:41:18 +0200

    Only add tweets newer than the most recently fetched stored in DB

--- bots/twitter_bot.py
+++ bots/twitter_bot.py
@@ -1,6 +1,6 @@
-import tweepy, settings, logging, time
+import tweepy, settings, logging, time, urllib.parse
 from datetime import datetime, timezone
-# from operator import itemgetter
+from operator import itemgetter
 
 # This solution is not going to use Cursors because of a potential memory consumption
 # issue. These scripts are goint to be running on an old Raspberry Pi, 
@@ -8,7 +8,7 @@ from datetime import datetime, timezone
 
 def get_twitter_items(since_id):
     api = get_twitter_api()
-    last_id = -1
+    last_id = 0
     searched_tweets = []
     retry_count = 0
 
@@ -16,10 +16,11 @@ def get_twitter_items(since_id):
         count = settings.TWITTER_MAX_TWEETS - len(searched_tweets)
         try:
             new_tweets = api.search(
+                # q=urllib.parse.quote(settings.TWITTER_QUERY, safe=''),
                 q=settings.TWITTER_QUERY, 
                 lang=settings.TWITTER_LANG,
                 # since_id=str(since_id),
-                max_id=str(last_id - 1), 
+                max_id=str(last_id-1),
                 count=count
             )
             if not new_tweets:
@@ -33,25 +34,29 @@ def get_twitter_items(since_id):
             logging.error('{}. Retrying in {} seconds. {} more retries.'.format(e, settings.TWITTER_RETRY_SLEEP, settings.TWITTER_RETRIES-retry_count))
             retry_count += 1
             time.sleep(settings.TWITTER_RETRY_SLEEP)
-    return format_tweets(searched_tweets)
+    return format_tweets(tweets=searched_tweets, since_id=since_id)
 
-def format_tweets(tweets):
+def format_tweets(tweets, since_id):
+    # Passing since_id is ugly, but the Twitter Search API doesn't play nice when max_id *and* since_id are provided,
+    # so this is a necessary, temporary workaround.
+    # GitHub issue link pending.
     formatted_tweets = [{
             'timestamp': t.created_at.replace(tzinfo=timezone.utc),
             'url': 'https://twitter.com/{user}/status/{status}'.format(
                 user=t.user.screen_name,
                 status=t.id_str),
-            'title': t.text.replace('\n', ' '),       # fuck people who put newlines in their tweets!
+            'title': t.text.replace('\n', ' '),       # fuck people who put newlines in their tweets! Fuck 'em!
             'twitter_id': t.id,
             'retweet_count': t.retweet_count,
             'favorite_count': t.favorite_count,
             'twitter_user': t.user.screen_name,
             'twitter_weight': t.retweet_count + t.favorite_count
-    } for t in tweets]
-    # Sorting isn't actually necessary here because the items only need to be sorted
-    # before display, not before committing.
-    # return sorted(formatted_tweets, key=itemgetter('weight'), reverse=True)
-    return formatted_tweets
+    } for t in tweets if t.id > since_id]
+
+    if settings.TWITTER_MAX_SAVED_TWEETS > 0:
+        return sorted(formatted_tweets, key=itemgetter(settings.TWITTER_SORT_KEY), reverse=True)[:settings.TWITTER_MAX_SAVED_TWEETS]
+    else:
+        return formatted_tweets
 
 def get_twitter_api():
     # OAthHandler includes too much unused functionality. All we are doing is searching
--- settings.py
+++ settings.py
@@ -36,9 +36,11 @@ TWITTER_QUERY = (
     'kleider OR shirt OR shirts)'
 )
 TWITTER_LANG = 'de'
-TWITTER_MAX_TWEETS = 100
-TWITTER_RETRIES = 3
+TWITTER_MAX_TWEETS = 1000
+TWITTER_RETRIES = 2
 TWITTER_RETRY_SLEEP = 10
+TWITTER_MAX_SAVED_TWEETS = 0       # set to 0 to save and publish all
+TWITTER_SORT_KEY = 'twitter_weight'
 
 LOG_LEVEL = logging.INFO
 MARK_PUBLISHED = True
